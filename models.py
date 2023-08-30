b6e3cd5611f6e725d5d0a819a9ec19859b057bef
aramiracle <alireza@arch-x1.localdomain>
2023-08-30 10:18:00 +0330

    temporary adding graphunet2 and pool and unpool

--- models.py
+++ models.py
@@ -5,47 +5,64 @@ from torch_geometric.nn import GINConv,TopKPooling, global_max_pool, global_mean
 import networkx as nx
 
 
-class GraphPoolingLayer(nn.Module):
-    def __init__(self, in_channels, ratio):
-        super(GraphPoolingLayer, self).__init__()
-        self.pool = TopKPooling(in_channels, ratio=ratio)
+import numpy as np
+import networkx as nx
+import torch
+import torch.nn as nn
 
-    def forward(self, x, edge_index, batch):
-        G = nx.Graph()
-        edges = edge_index.t().tolist()
-        G.add_edges_from(edges)
-        x, edge_index, _, batch, _, _ = self.pool(x, edge_index, None, batch)
-        return x, edge_index, batch
+class Pool(nn.Module):
+    def __init__(self, k, in_dim, p):
+        super(Pool, self).__init__()
+        self.k = k
+        self.centralities_num = 6
+        self.sigmoid = nn.Sigmoid()
+        self.feature_proj = nn.Linear(in_dim, 1)
+        self.structure_proj = nn.Linear(self.centralities_num, 1)
+        self.final_proj = nn.Linear(2, 1)
+        self.drop = nn.Dropout(p=p) if p > 0 else nn.Identity()
+
+    def forward(self, g, h):
+        Z = self.drop(h)
+        G = nx.from_numpy_array(g.detach().numpy())
+        C = all_centralities(G).float()
+        feature_weights = self.feature_proj(Z)
+        structure_weights = self.structure_proj(C)
+        weights = self.final_proj(torch.cat([feature_weights, structure_weights], dim=1)).squeeze()
+        scores = self.sigmoid(weights)
+        return top_k_graph(scores, g, h, self.k)
+
+class Unpool(nn.Module):
+    def forward(self, g, h, pre_h, idx):
+        new_h = torch.zeros_like(h)
+        new_h[idx] = h[idx]
+        idx_prime = torch.tensor([i for i in idx if i >= g.shape[0]])
+        for i in idx_prime:
+            new_h[i] = (h * g[i, :]).sum(0) / g[i, :].sum()
+        return g, new_h
 
 def centrality_based(centrality_metric, graph):
     if centrality_metric in ['closeness', 'degree', 'eigenvector', 'betweenness', 'load', 'subgraph', 'harmonic']:
-        centrality = nx.algorithms.centrality.__getattribute__(centrality_metric)(graph)
-        return torch.tensor(list(centrality.values()))
+        return torch.tensor(list(nx.algorithms.centrality.__getattribute__(centrality_metric)(graph).values()))
     else:
         raise ValueError("Unknown centrality metric")
 
 def all_centralities(graph):
-    return torch.stack([
-        centrality_based('closeness', graph),
-        centrality_based('degree', graph),
-        centrality_based('betweenness', graph),
-        centrality_based('load', graph),
-        centrality_based('subgraph', graph),
-        centrality_based('harmonic', graph)
-    ], dim=1)
+    return torch.stack([centrality_based(metric, graph) for metric in ['closeness', 'degree', 'betweenness', 'load', 'subgraph', 'harmonic']], dim=1)
 
-class GraphUnpoolingLayer(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(GraphUnpoolingLayer, self).__init__()
-        self.decoder = nn.Sequential(
-            nn.Linear(in_channels, out_channels),
-            nn.BatchNorm1d(out_channels),
-            nn.ReLU()
-        )
+def top_k_graph(scores, g, h, k):
+    num_nodes = g.shape[0]
+    values, idx = torch.topk(scores, max(2, int(k * num_nodes)))
+    new_h = h[idx, :]
+    values = torch.unsqueeze(values, -1)
+    new_h = torch.mul(new_h, values)
+    un_g = torch.matmul(g.bool().float(), torch.matmul(g.bool().float(), g.bool().float())).bool().float()
+    un_g = un_g[idx, :][:, idx]
+    g = norm_g(un_g)
+    return g, new_h, idx
 
-    def forward(self, x_pooled, x_previous):
-        x_unpooled = F.relu(self.decoder(x_pooled + x_previous))
-        return x_unpooled
+def norm_g(g):
+    return g / (g.sum(1, keepdim=True) + 1e-8)
+
 
 class GraphUNet2(torch.nn.Module):
     def __init__(self, num_features, num_classes):
@@ -59,7 +76,7 @@ class GraphUNet2(torch.nn.Module):
             nn.BatchNorm1d(32),
             nn.ReLU()
         ))
-        self.pool1 = GraphPoolingLayer(32, ratio=0.8)
+        self.pool1 = Pool(32, ratio=0.8)
         self.conv2 = GINConv(nn.Sequential(
             nn.Linear(32, 64),
             nn.BatchNorm1d(64),
@@ -68,7 +85,7 @@ class GraphUNet2(torch.nn.Module):
             nn.BatchNorm1d(64),
             nn.ReLU()
         ))
-        self.pool2 = GraphPoolingLayer(64, ratio=0.8)
+        self.pool2 = Pool(64, ratio=0.8)
         self.conv3 = GINConv(nn.Sequential(
             nn.Linear(64, 128),
             nn.BatchNorm1d(128),
@@ -77,7 +94,7 @@ class GraphUNet2(torch.nn.Module):
             nn.BatchNorm1d(128),
             nn.ReLU()
         ))
-        self.pool3 = GraphPoolingLayer(128, ratio=0.8)
+        self.pool3 = Pool(128, ratio=0.8)
 
         # Decoder
         self.decoder3 = nn.Sequential(
@@ -117,31 +134,10 @@ class GraphUNet2(torch.nn.Module):
         x_d2 = F.relu(self.decoder2(x_d3 + x2))
         x_d1 = F.log_softmax(self.decoder1(x_d2 + x1), dim=-1)
 
-        return x_d1Norm1d(32),
-           
-    def forward(self, data):
-        x, edge_index, batch = data.x, data.edge_index, data.batch
-
-        x = F.relu(self.conv1(x, edge_index))
-        x, edge_index, batch = self.pool1(x, edge_index, batch)
-        x1 = torch.cat([global_max_pool(x, batch), global_mean_pool(x, batch)], dim=1)
-
-        x = F.relu(self.conv2(x, edge_index))
-        x, edge_index, batch = self.pool2(x, edge_index, batch)
-        x2 = torch.cat([global_max_pool(x, batch), global_mean_pool(x, batch)], dim=1)
-
-        x = F.relu(self.conv3(x, edge_index))
-        x, edge_index, batch = self.pool3(x, edge_index, batch)
-        x3 = torch.cat([global_max_pool(x, batch), global_mean_pool(x, batch)], dim=1)
-
-        x_d3 = F.relu(self.decoder3(x3))
-        x_d2 = F.relu(self.decoder2(x_d3 + x2))
-        x_d1 = F.log_softmax(self.decoder1(x_d2 + x1), dim=-1)
-
         return x_d1
 
 
-class GraphUNet(torch.nn.Module):
+class GraphUNet(nn.Module):
     def __init__(self, num_features, num_classes):
         super(GraphUNet, self).__init__()
 
